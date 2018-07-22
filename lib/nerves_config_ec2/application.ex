dfc673860c84fc3e8e860008b3bb1859078e9483
Jake Morrison <jake@cogini.com>
2018-07-22 09:12:44 +0000

    Configure nerves_firmware_ssh authorized_keys from instance keypair

--- lib/nerves_config_ec2/application.ex
+++ lib/nerves_config_ec2/application.ex
@@ -6,6 +6,9 @@ defmodule NervesConfigEc2.Application do
   use Application
 
   def start(_type, _args) do
+
+    add_ssh_keys()
+
     # List all child processes to be supervised
     children = [
       # Starts a worker by calling: NervesConfigEc2.Worker.start_link(arg)
@@ -17,4 +20,19 @@ defmodule NervesConfigEc2.Application do
     opts = [strategy: :one_for_one, name: NervesConfigEc2.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  defp add_ssh_keys do
+    static = Application.get_env(:nerves_firmware_ssh, :authorized_keys, [])
+
+    instance = case :httpc.request('http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key') do
+      {:ok, {_, _, ""}} ->
+        []
+      {:ok, {_, _, key}} ->
+        [key]
+      _ ->
+        []
+    end
+    Application.put_env(:nerves_firmware_ssh, :authorized_keys, static ++ instance)
+  end
+
 end
