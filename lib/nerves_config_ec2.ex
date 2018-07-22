8e6252f3d6947d05002e11977e4abeec349c0111
Jake Morrison <jake@cogini.com>
2018-07-22 08:59:35 +0000

    Generate initial project

---
+++ lib/nerves_config_ec2.ex
@@ -0,0 +1,18 @@
+defmodule NervesConfigEc2 do
+  @moduledoc """
+  Documentation for NervesConfigEc2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesConfigEc2.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_config_ec2_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesConfigEc2Test do
+  use ExUnit.Case
+  doctest NervesConfigEc2
+
+  test "greets the world" do
+    assert NervesConfigEc2.hello() == :world
+  end
+end
